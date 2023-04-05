fd8919f61e13245f3510a222333d29a406f2d791
Chendra Kanth <80877964+Chendrakanth0105@users.noreply.github.com>
2023-04-05 18:27:10 +0530

    Lab 6 Update

--- Lab6/Lab6.java
+++ Lab6/Lab6.java
@@ -1,20 +1,53 @@
-import java.io.File;
+import java.io.*;
+import java.io.FileReader;
 import java.util.Scanner;
 
 class Lab6{
     public static void main(String[] args){
-        int a,e,i,o,u;
-        int count = 0;
+        int[] Vowels = new int[5];//[a,e,i,o,u]
         //Read file
         try{
-            File file = new File("./22122138-MDS273L-JAVA/Lab6/text.txt");
-            Scanner read = new Scanner(file);
-            while(read.hasNextLine()){
-                System.out.println(read.nextLine());
+            FileReader read = new FileReader("./22122138-MDS273L-JAVA/Lab6/text.txt");
+            int count;
+            while((count = read.read())!=-1){
+                char ch = (char) count;
+                String c = (String.valueOf(ch)).toLowerCase();
+                if(isVowel(c)){
+                    switch(c){
+                        case "a":
+                            Vowels[0]++;
+                            break;
+                        case "e":
+                            Vowels[1]++;
+                            break;
+                        case "i":
+                            Vowels[2]++;
+                            break;
+                        case "o":
+                            Vowels[3]++;
+                            break;
+                        case "u":
+                            Vowels[4]++;
+                            break;
+
+                    }
+                }
             }
+            read.close();
+        System.out.println("Vowel counts:");
+        System.out.println("a: " + Vowels[0]);
+        System.out.println("e: " + Vowels[1]);
+        System.out.println("i: " + Vowels[2]);
+        System.out.println("o: " + Vowels[3]);
+        System.out.println("u: " + Vowels[4]);
+
         }catch(Exception exc){
             System.out.println(exc.getMessage());
         }
         
+    }
+    public static boolean isVowel(String ch){
+        String vowel = "aeiou";
+        return vowel.indexOf(ch) != -1;
     }
 }
---
+++ Lab6/README.md
@@ -0,0 +1,8 @@
+QUESTION: <br>
+Write a JAVA program that reads a file (text file) and finds the following details.<br>
+Vowels and their count. Eg: [a:100, e:45, I:55, ..]<br>
+Digits and their count. Eg: [0:1000, 1:10, 2:40, ...]<br>
+Top Five repeated words and their count.<br>
+Least Five repeated words and their count.<br>
+Top Five repeated characters and their count.<br>
+Least FIve repeated characters and their count.<br>
